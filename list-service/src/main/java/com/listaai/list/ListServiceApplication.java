d51adbf0c757fe36c90a6fb04712eb62aeb5e9fb
eduardo-toste <duciochetti@gmail.com>
2026-04-08 12:23:21 -0300

    feat: initialize list-service project structure

---
+++ list-service/src/main/java/com/listaai/list/ListServiceApplication.java
@@ -0,0 +1,13 @@
+package com.listaai.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListServiceApplication.class, args);
+	}
+
+}
---
+++ list-service/src/test/java/com/listaai/list/ListServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listaai.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
