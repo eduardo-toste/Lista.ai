21def2ece755f9ed0eb6e45cddd08c7d6cbbdb36
eduardo-toste <duciochetti@gmail.com>
2026-04-13 07:45:26 -0300

    feat: implement CreateShoppingListService for creating shopping lists

---
+++ list-service/src/main/java/com/listaai/list/application/usecase/CreateShoppingListService.java
@@ -0,0 +1,27 @@
+package com.listaai.list.application.usecase;
+
+import com.listaai.list.application.dto.input.CreateShoppingListCommand;
+import com.listaai.list.application.dto.output.ShoppingListOutput;
+import com.listaai.list.application.mapper.ShoppingListMapper;
+import com.listaai.list.application.port.inbound.CreateShoppingListUseCase;
+import com.listaai.list.application.port.outbound.ShoppingListRepositoryPort;
+import com.listaai.list.domain.model.ShoppingList;
+
+public class CreateShoppingListService implements CreateShoppingListUseCase {
+
+    private ShoppingListRepositoryPort shoppingListRepositoryPort;
+    private ShoppingListMapper shoppingListMapper;
+
+    public CreateShoppingListService(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper) {
+        this.shoppingListRepositoryPort = shoppingListRepositoryPort;
+        this.shoppingListMapper = shoppingListMapper;
+    }
+
+    @Override
+    public ShoppingListOutput createShoppingList(CreateShoppingListCommand command) {
+        ShoppingList shoppingList = shoppingListMapper.toDomain(command);
+        ShoppingList savedShoppingList = shoppingListRepositoryPort.save(shoppingList);
+        return shoppingListMapper.toOutput(savedShoppingList);
+    }
+
+}
