e4366c43c66459616278656db20a115b66299785
eduardo-toste <duciochetti@gmail.com>
2026-04-13 20:55:07 -0300

    feat: add @Builder annotation to entity classes for improved object creation

--- list-service/src/main/java/com/listaai/list/adapter/outbound/persistence/entity/ShoppingListItemEntity.java
+++ list-service/src/main/java/com/listaai/list/adapter/outbound/persistence/entity/ShoppingListItemEntity.java
@@ -2,10 +2,7 @@ package com.listaai.list.adapter.outbound.persistence.entity;
 
 import com.listaai.list.domain.enums.ItemUnit;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "items")
@@ -13,6 +10,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class ShoppingListItemEntity {
 
     @Id
