e3e77eec020e1c1b3636b3020f87bfda1f031ea3
eduardo-toste <duciochetti@gmail.com>
2026-04-14 23:25:28 -0300

    feat: add GetShoppingListUseCase bean for retrieving shopping lists

--- list-service/src/main/java/com/listaai/list/configuration/ShoppingListUseCaseConfig.java
+++ list-service/src/main/java/com/listaai/list/configuration/ShoppingListUseCaseConfig.java
@@ -2,8 +2,10 @@ package com.listaai.list.configuration;
 
 import com.listaai.list.application.mapper.ShoppingListMapper;
 import com.listaai.list.application.port.inbound.CreateShoppingListUseCase;
+import com.listaai.list.application.port.inbound.GetShoppingListUseCase;
 import com.listaai.list.application.port.outbound.ShoppingListRepositoryPort;
 import com.listaai.list.application.usecase.CreateShoppingListService;
+import com.listaai.list.application.usecase.GetShoppingListService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,11 @@ public class ShoppingListUseCaseConfig {
     @Bean
     public CreateShoppingListUseCase createShoppingListUseCase(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper) {
         return new CreateShoppingListService(shoppingListRepositoryPort, shoppingListMapper);
+    }
+
+    @Bean
+    public GetShoppingListUseCase getShoppingListUseCase(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper) {
+        return new GetShoppingListService(shoppingListRepositoryPort, shoppingListMapper);
     }
 
 }
