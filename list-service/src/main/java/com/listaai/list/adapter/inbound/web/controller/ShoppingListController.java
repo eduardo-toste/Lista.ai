59d6b2464d7cd6f879dbe6e40ba08c26ec9d1b28
eduardo-toste <duciochetti@gmail.com>
2026-04-14 23:25:45 -0300

    feat: add endpoints to retrieve shopping lists by ID and with pagination

--- list-service/src/main/java/com/listaai/list/adapter/inbound/web/controller/ShoppingListController.java
+++ list-service/src/main/java/com/listaai/list/adapter/inbound/web/controller/ShoppingListController.java
@@ -6,17 +6,21 @@ import com.listaai.list.adapter.inbound.web.response.ShoppingListResponse;
 import com.listaai.list.application.dto.input.CreateShoppingListCommand;
 import com.listaai.list.application.dto.output.ShoppingListOutput;
 import com.listaai.list.application.port.inbound.CreateShoppingListUseCase;
+import com.listaai.list.application.port.inbound.GetShoppingListUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/list")
+@RequestMapping("/lists")
 @RequiredArgsConstructor
 public class ShoppingListController {
 
     private final CreateShoppingListUseCase createShoppingListUseCase;
+    private final GetShoppingListUseCase getShoppingListUseCase;
     private final ShoppingListWebMapper shoppingListMapper;
 
     @PostMapping
@@ -27,4 +31,17 @@ public class ShoppingListController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<ShoppingListResponse> getShoppingListById(@PathVariable Long id) {
+        ShoppingListOutput output = getShoppingListUseCase.getShoppingListById(id);
+        ShoppingListResponse response = shoppingListMapper.toResponse(output);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ShoppingListResponse>> getShoppingLists(Pageable pageable) {
+        Page<ShoppingListOutput> pageOutput = getShoppingListUseCase.getShoppingLists(pageable);
+        Page<ShoppingListResponse> pageResponse = shoppingListMapper.toPageResponse(pageOutput);
+        return ResponseEntity.ok(pageResponse);
+    }
 }
