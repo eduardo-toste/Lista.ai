dbd2bdfc6223abb15d6c73800e1537a5a77a2560
eduardo-toste <duciochetti@gmail.com>
2026-04-15 20:04:11 -0300

    feat: add UpdateListNameUseCase bean for updating shopping list names

--- list-service/src/main/java/com/listaai/list/configuration/ShoppingListUseCaseConfig.java
+++ list-service/src/main/java/com/listaai/list/configuration/ShoppingListUseCaseConfig.java
@@ -3,9 +3,11 @@ package com.listaai.list.configuration;
 import com.listaai.list.application.mapper.ShoppingListMapper;
 import com.listaai.list.application.port.inbound.CreateShoppingListUseCase;
 import com.listaai.list.application.port.inbound.GetShoppingListUseCase;
+import com.listaai.list.application.port.inbound.UpdateListNameUseCase;
 import com.listaai.list.application.port.outbound.ShoppingListRepositoryPort;
 import com.listaai.list.application.usecase.CreateShoppingListService;
 import com.listaai.list.application.usecase.GetShoppingListService;
+import com.listaai.list.application.usecase.UpdateListNameService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,6 +22,11 @@ public class ShoppingListUseCaseConfig {
     @Bean
     public GetShoppingListUseCase getShoppingListUseCase(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper) {
         return new GetShoppingListService(shoppingListRepositoryPort, shoppingListMapper);
+    }
+
+    @Bean
+    public UpdateListNameUseCase updateListNameUseCase(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper) {
+        return new UpdateListNameService(shoppingListRepositoryPort, shoppingListMapper);
     }
 
 }
