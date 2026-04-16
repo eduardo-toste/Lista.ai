e569e198c0e6f08b8c2546b6b31aff9ba430a48d
eduardo-toste <duciochetti@gmail.com>
2026-04-15 22:50:06 -0300

    feat: implement AddItemToListService for adding items to shopping lists

---
+++ list-service/src/main/java/com/listaai/list/application/usecase/items/AddItemToListService.java
@@ -0,0 +1,38 @@
+package com.listaai.list.application.usecase.items;
+
+import com.listaai.list.application.dto.input.ShoppingListItemCommand;
+import com.listaai.list.application.dto.output.ShoppingListOutput;
+import com.listaai.list.application.exception.ShoppingListNotFoundException;
+import com.listaai.list.application.mapper.ShoppingListItemMapper;
+import com.listaai.list.application.mapper.ShoppingListMapper;
+import com.listaai.list.application.port.inbound.items.AddItemToListUseCase;
+import com.listaai.list.application.port.outbound.ShoppingListRepositoryPort;
+import com.listaai.list.domain.model.ShoppingList;
+import com.listaai.list.domain.model.ShoppingListItem;
+
+public class AddItemToListService implements AddItemToListUseCase {
+
+    private final ShoppingListRepositoryPort shoppingListRepositoryPort;
+    private final ShoppingListMapper shoppingListMapper;
+    private final ShoppingListItemMapper shoppingListItemMapper;
+
+    public AddItemToListService(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper, ShoppingListItemMapper shoppingListItemMapper) {
+        this.shoppingListRepositoryPort = shoppingListRepositoryPort;
+        this.shoppingListMapper = shoppingListMapper;
+        this.shoppingListItemMapper = shoppingListItemMapper;
+    }
+
+    @Override
+    public ShoppingListOutput addItemToShoppingList(Long listId, ShoppingListItemCommand command) {
+        ShoppingList shoppingList = shoppingListRepositoryPort.findById(listId)
+                .orElseThrow(ShoppingListNotFoundException::new);
+
+        ShoppingListItem item = shoppingListItemMapper.toDomain(command);
+
+        shoppingList.addItem(item);
+        ShoppingList savedShoppingList = shoppingListRepositoryPort.save(shoppingList);
+
+        return shoppingListMapper.toOutput(savedShoppingList);
+    }
+
+}
