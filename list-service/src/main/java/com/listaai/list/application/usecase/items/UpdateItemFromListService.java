5f807857736e1e6232a97e411ceb815a569fb471
eduardo-toste <duciochetti@gmail.com>
2026-04-16 14:24:52 -0300

    feat: implement UpdateItemFromListService for updating items in shopping lists

---
+++ list-service/src/main/java/com/listaai/list/application/usecase/items/UpdateItemFromListService.java
@@ -0,0 +1,32 @@
+package com.listaai.list.application.usecase.items;
+
+import com.listaai.list.application.dto.input.ShoppingListItemCommand;
+import com.listaai.list.application.dto.output.ShoppingListOutput;
+import com.listaai.list.application.exception.ShoppingListNotFoundException;
+import com.listaai.list.application.mapper.ShoppingListMapper;
+import com.listaai.list.application.port.inbound.items.UpdateItemFromListUseCase;
+import com.listaai.list.application.port.outbound.ShoppingListRepositoryPort;
+import com.listaai.list.domain.model.ShoppingList;
+
+public class UpdateItemFromListService implements UpdateItemFromListUseCase {
+
+    private final ShoppingListRepositoryPort shoppingListRepositoryPort;
+    private final ShoppingListMapper shoppingListMapper;
+
+    public UpdateItemFromListService(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper) {
+        this.shoppingListRepositoryPort = shoppingListRepositoryPort;
+        this.shoppingListMapper = shoppingListMapper;
+    }
+
+    @Override
+    public ShoppingListOutput updateItem(Long listId, Long itemId, ShoppingListItemCommand command) {
+        ShoppingList shoppingList = shoppingListRepositoryPort.findById(listId)
+                .orElseThrow(ShoppingListNotFoundException::new);
+
+        shoppingList.updateItem(itemId, command.name(), command.quantity(), command.unit());
+        ShoppingList savedShoppingList = shoppingListRepositoryPort.save(shoppingList);
+
+        return shoppingListMapper.toOutput(savedShoppingList);
+    }
+
+}
