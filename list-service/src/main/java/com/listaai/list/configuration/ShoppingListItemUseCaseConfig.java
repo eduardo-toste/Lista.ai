6706203fa08cefe8b0b6d0bf30fdc40406710fd1
eduardo-toste <duciochetti@gmail.com>
2026-04-16 13:59:55 -0300

    feat: add RemoveItemFromListUseCase bean configuration in ShoppingListItemUseCaseConfig

--- list-service/src/main/java/com/listaai/list/configuration/ShoppingListItemUseCaseConfig.java
+++ list-service/src/main/java/com/listaai/list/configuration/ShoppingListItemUseCaseConfig.java
@@ -3,8 +3,10 @@ package com.listaai.list.configuration;
 import com.listaai.list.application.mapper.ShoppingListItemMapper;
 import com.listaai.list.application.mapper.ShoppingListMapper;
 import com.listaai.list.application.port.inbound.items.AddItemToListUseCase;
+import com.listaai.list.application.port.inbound.items.RemoveItemFromListUseCase;
 import com.listaai.list.application.port.outbound.ShoppingListRepositoryPort;
 import com.listaai.list.application.usecase.items.AddItemToListService;
+import com.listaai.list.application.usecase.items.RemoveItemFromListService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +16,11 @@ public class ShoppingListItemUseCaseConfig {
     @Bean
     public AddItemToListUseCase addItemToListUseCase(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper, ShoppingListItemMapper shoppingListItemMapper) {
         return new AddItemToListService(shoppingListRepositoryPort, shoppingListMapper, shoppingListItemMapper);
+    }
+
+    @Bean
+    public RemoveItemFromListUseCase removeItemFromListUseCase(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper, ShoppingListItemMapper shoppingListItemMapper) {
+        return new RemoveItemFromListService(shoppingListRepositoryPort, shoppingListMapper, shoppingListItemMapper);
     }
 
 }
