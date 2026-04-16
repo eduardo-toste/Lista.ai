3bb850d94d6ac40bc1bbcc3ea9ebc9a65ca40343
eduardo-toste <duciochetti@gmail.com>
2026-04-16 14:25:14 -0300

    feat: add updateItemFromList method to ShoppingListItemController for updating items

--- list-service/src/main/java/com/listaai/list/adapter/inbound/web/controller/ShoppingListItemController.java
+++ list-service/src/main/java/com/listaai/list/adapter/inbound/web/controller/ShoppingListItemController.java
@@ -3,10 +3,12 @@ package com.listaai.list.adapter.inbound.web.controller;
 import com.listaai.list.adapter.inbound.web.mapper.ShoppingListItemWebMapper;
 import com.listaai.list.adapter.inbound.web.mapper.ShoppingListWebMapper;
 import com.listaai.list.adapter.inbound.web.request.CreateShoppingListItemRequest;
+import com.listaai.list.adapter.inbound.web.request.UpdateShoppingListItemRequest;
 import com.listaai.list.adapter.inbound.web.response.ShoppingListResponse;
 import com.listaai.list.application.dto.output.ShoppingListOutput;
 import com.listaai.list.application.port.inbound.items.AddItemToListUseCase;
 import com.listaai.list.application.port.inbound.items.RemoveItemFromListUseCase;
+import com.listaai.list.application.port.inbound.items.UpdateItemFromListUseCase;
 import com.listaai.list.domain.model.ShoppingList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,7 @@ public class ShoppingListItemController {
 
     private final AddItemToListUseCase addItemToListUseCase;
     private final RemoveItemFromListUseCase removeItemFromListUseCase;
+    private final UpdateItemFromListUseCase updateItemFromListUseCase;
     private final ShoppingListItemWebMapper shoppingListItemWebMapper;
     private final ShoppingListWebMapper shoppingListWebMapper;
 
@@ -32,6 +35,13 @@ public class ShoppingListItemController {
     @DeleteMapping("{itemId}")
     public ResponseEntity<ShoppingListResponse> removeItemFromList(@PathVariable Long listId, @PathVariable Long itemId) {
         ShoppingListOutput output = removeItemFromListUseCase.removeItemFromShoppingList(listId, itemId);
+        ShoppingListResponse response = shoppingListWebMapper.toResponse(output);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("{itemId}")
+    public ResponseEntity<ShoppingListResponse> updateItemFromList(@PathVariable Long listId, @PathVariable Long itemId, @RequestBody UpdateShoppingListItemRequest request) {
+        ShoppingListOutput output = updateItemFromListUseCase.updateItem(listId, itemId, shoppingListItemWebMapper.toCommand(request));
         ShoppingListResponse response = shoppingListWebMapper.toResponse(output);
         return ResponseEntity.ok(response);
     }
