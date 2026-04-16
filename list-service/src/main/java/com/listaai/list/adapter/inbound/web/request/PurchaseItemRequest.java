8d1009ddaf2e9db4e12cdfb5e70fb0d4f62be984
eduardo-toste <duciochetti@gmail.com>
2026-04-16 15:44:31 -0300

    feat: add PurchaseItemRequest record for item purchase status

---
+++ list-service/src/main/java/com/listaai/list/adapter/inbound/web/request/PurchaseItemRequest.java
@@ -0,0 +1,12 @@
+package com.listaai.list.adapter.inbound.web.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record PurchaseItemRequest(
+
+        @NotNull(message = "Purchase status must not be null")
+        boolean purchased
+
+) {
+}
