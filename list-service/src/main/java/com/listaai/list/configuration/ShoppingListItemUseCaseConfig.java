46add4b6b7549b6f7f93e1fa4236ad70161d8dfb
eduardo-toste <duciochetti@gmail.com>
2026-04-16 15:44:27 -0300

    feat: add PurchaseItemFromListUseCase bean for purchasing items from the shopping list

--- list-service/src/main/java/com/listaai/list/configuration/ShoppingListItemUseCaseConfig.java
+++ list-service/src/main/java/com/listaai/list/configuration/ShoppingListItemUseCaseConfig.java
@@ -3,10 +3,12 @@ package com.listaai.list.configuration;
 import com.listaai.list.application.mapper.ShoppingListItemMapper;
 import com.listaai.list.application.mapper.ShoppingListMapper;
 import com.listaai.list.application.port.inbound.items.AddItemToListUseCase;
+import com.listaai.list.application.port.inbound.items.PurchaseItemFromListUseCase;
 import com.listaai.list.application.port.inbound.items.RemoveItemFromListUseCase;
 import com.listaai.list.application.port.inbound.items.UpdateItemFromListUseCase;
 import com.listaai.list.application.port.outbound.ShoppingListRepositoryPort;
 import com.listaai.list.application.usecase.items.AddItemToListService;
+import com.listaai.list.application.usecase.items.PurchaseItemFromListService;
 import com.listaai.list.application.usecase.items.RemoveItemFromListService;
 import com.listaai.list.application.usecase.items.UpdateItemFromListService;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +30,11 @@ public class ShoppingListItemUseCaseConfig {
     @Bean
     public UpdateItemFromListUseCase updateItemFromListUseCase(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper) {
         return new UpdateItemFromListService(shoppingListRepositoryPort, shoppingListMapper);
+    }
+
+    @Bean
+    public PurchaseItemFromListUseCase purchaseItemFromListUseCase(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper) {
+        return new PurchaseItemFromListService(shoppingListRepositoryPort, shoppingListMapper);
     }
 
 }
