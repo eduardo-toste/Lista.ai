5eec1a1752b694ad6c6f0163991da9d69052a083
eduardo-toste <duciochetti@gmail.com>
2026-04-19 22:08:58 -0300

    feat: add configuration for Update and Remove Participant use cases in shopping lists

--- list-service/src/main/java/com/listaai/list/configuration/ShoppingListParticipantUseCaseConfig.java
+++ list-service/src/main/java/com/listaai/list/configuration/ShoppingListParticipantUseCaseConfig.java
@@ -3,8 +3,12 @@ package com.listaai.list.configuration;
 import com.listaai.list.application.mapper.ShoppingListMapper;
 import com.listaai.list.application.mapper.ShoppingListParticipantMapper;
 import com.listaai.list.application.port.inbound.participants.AddParticipantToListUseCase;
+import com.listaai.list.application.port.inbound.participants.RemoveParticipantFromListUseCase;
+import com.listaai.list.application.port.inbound.participants.UpdateParticipantFromListUseCase;
 import com.listaai.list.application.port.outbound.ShoppingListRepositoryPort;
 import com.listaai.list.application.usecase.participants.AddParticipantToListService;
+import com.listaai.list.application.usecase.participants.RemoveParticipantFromListService;
+import com.listaai.list.application.usecase.participants.UpdateParticipantFromListService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +18,16 @@ public class ShoppingListParticipantUseCaseConfig {
     @Bean
     public AddParticipantToListUseCase addParticipantToListUseCase(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper, ShoppingListParticipantMapper shoppingListParticipantMapper) {
         return new AddParticipantToListService(shoppingListRepositoryPort, shoppingListMapper, shoppingListParticipantMapper);
+    }
+
+    @Bean
+    public UpdateParticipantFromListUseCase updateParticipantFromListUseCase(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper) {
+        return new UpdateParticipantFromListService(shoppingListRepositoryPort, shoppingListMapper);
+    }
+
+    @Bean
+    public RemoveParticipantFromListUseCase removeParticipantFromListUseCase(ShoppingListRepositoryPort shoppingListRepositoryPort, ShoppingListMapper shoppingListMapper) {
+        return new RemoveParticipantFromListService(shoppingListRepositoryPort, shoppingListMapper);
     }
 
 }
