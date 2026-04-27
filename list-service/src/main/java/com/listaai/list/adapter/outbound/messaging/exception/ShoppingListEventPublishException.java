3b04b6b3466ef93a2c78ad99dcdbfef238d6d0eb
eduardo-toste <duciochetti@gmail.com>
2026-04-27 17:08:12 -0300

    feat: add shopping list publish custom exception

---
+++ list-service/src/main/java/com/listaai/list/adapter/outbound/messaging/exception/ShoppingListEventPublishException.java
@@ -0,0 +1,8 @@
+package com.listaai.list.adapter.outbound.messaging.exception;
+
+public class ShoppingListEventPublishException extends RuntimeException {
+
+    public ShoppingListEventPublishException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
