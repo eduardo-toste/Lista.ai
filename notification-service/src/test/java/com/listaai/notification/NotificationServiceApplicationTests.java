47987953d2552f5407f4d9b89413ab9cafa74e59
eduardo-toste <duciochetti@gmail.com>
2026-04-28 14:34:26 -0300

    feat: add notification service to project

---
+++ notification-service/src/test/java/com/listaai/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listaai.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
