04e2789b85009fd52d815817d4fa84ee8180c4d5
eduardo-toste <duciochetti@gmail.com>
2026-04-28 15:55:00 -0300

    feat: implement handle share shopping list service logic

--- notification-service/src/main/java/com/listaai/notification/application/usecase/HandleShoppingListSharedService.java
+++ notification-service/src/main/java/com/listaai/notification/application/usecase/HandleShoppingListSharedService.java
@@ -1,21 +1,34 @@
 package com.listaai.notification.application.usecase;
 
 import com.listaai.notification.application.dto.input.HandleShoppingListSharedCommand;
+import com.listaai.notification.application.dto.output.NotificationRequest;
+import com.listaai.notification.application.mapper.NotificationMapper;
 import com.listaai.notification.application.port.inbound.HandleShoppingListSharedUseCase;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.listaai.notification.application.port.outbound.NotificationSenderPort;
 
 public class HandleShoppingListSharedService implements HandleShoppingListSharedUseCase {
 
-    private static final Logger log = LoggerFactory.getLogger(HandleShoppingListSharedService.class);
+    private final NotificationSenderPort notificationSenderPort;
+    private final NotificationMapper notificationMapper;
+
+    public HandleShoppingListSharedService(
+            NotificationSenderPort notificationSenderPort,
+            NotificationMapper notificationMapper
+    ) {
+        this.notificationSenderPort = notificationSenderPort;
+        this.notificationMapper = notificationMapper;
+    }
 
     @Override
     public void handle(HandleShoppingListSharedCommand command) {
-        log.info(
-                "Received shopping list shared event: listId={}, listName={}, participants={}",
-                command.shoppingListId(),
-                command.shoppingListName(),
-                command.participants().size()
-        );
+        command.participants().forEach(participant -> {
+            NotificationRequest request = notificationMapper.toRequest(
+                    "WHATSAPP",
+                    "shopping-list-shared",
+                    participant,
+                    command
+            );
+            notificationSenderPort.send(request);
+        });
     }
 }
